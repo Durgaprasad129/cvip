a0da6cb6f1da5ab7afab430e402346e6ce0a1e10
Kayala Durgaprasad <117065086+Durgaprasad129@users.noreply.github.com>
2023-09-04 17:18:11 +0530

    Add files via upload

---
+++ Netflix/clone.js
@@ -0,0 +1,8 @@
+
+function newFunction() {
+    let startBtn = document.getElementById("startBtn");
+
+    startBtn.onclick = function () {
+        location.href = "http://127.0.0.1:5500/page-navigation.html";
+    };
+}
